10c270f93c454497f11209d600266152fa72ff98
Abir <abirgpx@gmail.com>
2024-11-28 20:45:12 +0600

    hi

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function showTime() {
 
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
-  m = m < 10 ? "0" + s : s;
+  s = s < 10 ? "0" + s : s;
 
   var time = `${h}:${m}:${s} ${session}`;
 
